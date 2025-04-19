d4dffee3734a5882c72dbd1de8211141192f4e60
hoangcao21 <hoangcao21@github.com>
2025-04-19 20:36:28 +0700

    chore: update user-related actors

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,16 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 
-export class UserRepository extends Repository<UserEntity> {
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly repo: Repository<UserEntity>,
+  ) {}
+
   async findOneByUserName(userName: string): Promise<UserEntity | null> {
-    const userEntity = await this.findOne({ where: { userName } });
+    const userEntity = await this.repo.findOne({ where: { userName } });
 
     return userEntity;
   }
 
   async findOneByUserId(userId: string): Promise<UserEntity | null> {
-    const userEntity = await this.findOne({ where: { id: userId } });
+    const userEntity = await this.repo.findOne({ where: { id: userId } });
 
     return userEntity;
+  }
+
+  async createOne(userEntity: UserEntity): Promise<UserEntity> {
+    return this.repo.save(userEntity);
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { UserEntity } from './user.entity';
 import { UserRepository } from './user.repository';
 
+@Injectable()
 export class UserService {
   constructor(private readonly userRepo: UserRepository) {}
 
@@ -10,5 +12,16 @@ export class UserService {
 
   async getUserById(userId: string): Promise<UserEntity | null> {
     return this.userRepo.findOneByUserId(userId);
+  }
+
+  async createUser(
+    userName: string,
+    hashedPassword: string,
+  ): Promise<UserEntity | null> {
+    const userEntity: UserEntity = new UserEntity();
+    userEntity.userName = userName;
+    userEntity.hashedPassword = hashedPassword;
+
+    return this.userRepo.createOne(userEntity);
   }
 }
