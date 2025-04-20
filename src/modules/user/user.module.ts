456805be0780d0118613d3462f263f586a44bbf5
hoangcao21 <hoangcao21@github.com>
2025-04-20 14:56:37 +0700

    chore: use cache and kafka client in user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,11 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
+import { UserCacheService } from './user-cache.service';
+import { PostServiceKafkaClientModule } from 'src/shared/message-broker/client/post-service-kafka-client.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    PostServiceKafkaClientModule,
+  ],
   controllers: [],
-  providers: [UserRepository, UserService],
+  providers: [UserRepository, UserService, UserCacheService],
   exports: [UserService, UserRepository],
 })
 export class UserModule {}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,27 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { UserCacheService } from './user-cache.service';
 import { UserEntity } from './user.entity';
 import { UserRepository } from './user.repository';
+import { PostServiceKafkaClientService } from 'src/shared/message-broker/client/post-service-kafka-client.service';
+import { UserCreatedEvent } from './events/user-created.event';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepo: UserRepository) {}
+  constructor(
+    private readonly userRepo: UserRepository,
+    private readonly userCacheService: UserCacheService,
+    private readonly postServiceKafkaClientService: PostServiceKafkaClientService,
+  ) {}
 
   async getUserByUserName(userName: string): Promise<UserEntity | null> {
-    return this.userRepo.findOneByUserName(userName);
+    let userEntity = await this.userCacheService.getUser(userName);
+
+    if (!userEntity) {
+      userEntity = await this.userRepo.findOneByUserName(userName);
+
+      if (!userEntity) {
+        return null;
+      }
+
+      await this.userCacheService.setUser(userEntity);
+    }
+
+    return userEntity;
   }
 
   async getUserById(userId: string): Promise<UserEntity | null> {
-    return this.userRepo.findOneByUserId(userId);
+    let userEntity = await this.userCacheService.getUser(userId);
+
+    if (!userEntity) {
+      userEntity = await this.userRepo.findOneByUserId(userId);
+
+      if (!userEntity) {
+        return null;
+      }
+
+      await this.userCacheService.setUser(userEntity);
+    }
+
+    return userEntity;
   }
 
   async createUser(
     userName: string,
     hashedPassword: string,
   ): Promise<UserEntity | null> {
-    const userEntity: UserEntity = new UserEntity();
+    let userEntity: UserEntity = new UserEntity();
     userEntity.userName = userName;
     userEntity.hashedPassword = hashedPassword;
 
-    return this.userRepo.createOne(userEntity);
+    userEntity = await this.userRepo.createOne(userEntity);
+
+    this.postServiceKafkaClientService.sendUserCreated(
+      new UserCreatedEvent(userEntity.id, userEntity.createdAt.toUTCString()),
+    );
+
+    return userEntity;
   }
 }
